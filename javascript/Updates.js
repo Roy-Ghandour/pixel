098fe573dfa8f2249ea624c91fd6f4ce11da042e
Roy Ghandour <ghandourroy@gmail.com>
2019-08-12 01:08:38 +0400

    Refined final details. Check changelog in javascript/Update.js for more. v1.4

---
+++ javascript/Updates.js
@@ -0,0 +1,36 @@
+//Update alert for v1.4
+//Will be commented for testing pruposes of v1.5
+alert("Update v1.4:\n"+
+    "There is no longer any need to refresh the page! Try out\n"+
+    "the space bar to get different results! Also try clicking\n"+
+    "the sun button ;)");
+
+/*
+Change Log:
+    Update v1.1:
+        Added spacebar to change cube_color1 and cube_color2
+        Added the update message
+    Update v1.2:
+        Removed spacebar to change cube_color1 and cube_color2
+        Added spacebar to refresh the results of the page
+        Added the ToolKit.js file for cleaning
+        Added general comments
+        Added changelog
+        Increased num_of_cubes from 140 to 160
+        Reformatted script
+    Update v1.3:
+        Added previous mosue position tracking to make transion more fluid
+        Added comments to all scripts
+        Added spacebar prompt
+        Reformatted night_toggle and header
+        Organised file structure
+        Organised scripts
+    Update v1.4:
+        Refined prompt css
+        Organised update scripts
+*/
+
+/*
+Plans for update v1.5: (^ = completed)
+
+*/
